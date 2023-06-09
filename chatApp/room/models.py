c3fc6adcda6722c5fe1e503335b6ec2704433b0f
jb2467 <jb2467@rit.edu>
2023-06-08 22:54:03 -0500

    Can view the rooms of the app

--- chatApp/room/models.py
+++ chatApp/room/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
 
 class Room(models.Model):
     name = models.CharField(max_length=255)
--- chatApp/room/views.py
+++ chatApp/room/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .models import Room
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+
+    return render(request, 'room/rooms.html', {'rooms': rooms})
 # Create your views here.
